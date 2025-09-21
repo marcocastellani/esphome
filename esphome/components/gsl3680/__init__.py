f0c8cfc8797de298d703906d56e325792c84edcc
Marco Castellani <marcocastellani@hotmail.com>
2025-09-21 18:37:16 +0200

    Fix GSL3680 touchscreen Python files: correct pin schema and add CODEOWNERS

--- esphome/components/gsl3680/__init__.py
+++ esphome/components/gsl3680/__init__.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID
 
+CODEOWNERS = ["@marcocastellani"]
 DEPENDENCIES = ["i2c"]
 
 gsl3680_ns = cg.esphome_ns.namespace("gsl3680")
--- esphome/components/gsl3680/touchscreen.py
+++ esphome/components/gsl3680/touchscreen.py
@@ -1,3 +1,4 @@
+from esphome import pins
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import touchscreen, i2c
@@ -7,6 +8,7 @@ from esphome.const import (
     CONF_RESET_PIN,
 )
 
+CODEOWNERS = ["@marcocastellani"]
 DEPENDENCIES = ["i2c"]
 
 gsl3680_ns = cg.esphome_ns.namespace("gsl3680")
@@ -17,8 +19,8 @@ GSL3680Touchscreen = gsl3680_ns.class_(
 CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(GSL3680Touchscreen),
-        cv.Required(CONF_INTERRUPT_PIN): cv.pin_schema,
-        cv.Optional(CONF_RESET_PIN): cv.pin_schema,
+        cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
+        cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
     }
 ).extend(i2c.i2c_device_schema(0x40))
 
@@ -28,9 +30,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
     
-    interrupt_pin = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
-    cg.add(var.set_interrupt_pin(interrupt_pin))
-    
-    if CONF_RESET_PIN in config:
-        reset_pin = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
-        cg.add(var.set_reset_pin(reset_pin))
+    if interrupt_pin := config.get(CONF_INTERRUPT_PIN):
+        cg.add(var.set_interrupt_pin(await cg.gpio_pin_expression(interrupt_pin)))
+    if reset_pin := config.get(CONF_RESET_PIN):
+        cg.add(var.set_reset_pin(await cg.gpio_pin_expression(reset_pin)))
